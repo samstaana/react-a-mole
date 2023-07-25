1ca76401c541b3a24c49a979c42d8146d3387816
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-07-24 20:35:43 -0700

    generating MoleContainers

--- src/Mole.js
+++ src/Mole.js
@@ -1,9 +1,22 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
+import moleImg from './images/mole.png'
 
-function Mole () {
+function Mole(props) {
+    useEffect(() => {
+        // Feel fress to adjust this number
+        let randSeconds = Math.ceil(Math.random() * 5500)
+        let timer = setTimeout(() => {
+            props.setDisplayMode(false)
+        }, randSeconds)
+        return() => clearTimeout(timer)
+    })
+
     return (
         <div>
-            <h2>Mole</h2>
+            <img 
+            style = {{ 'width': '30vw' }}
+            src = { moleImg }
+            onClick = { props.handleClick } />
         </div>
     )
 }
