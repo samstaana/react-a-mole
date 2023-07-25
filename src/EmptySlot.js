6deba6578de4e64291ddeee5682ed1b7255a2ead
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-07-24 20:24:26 -0700

    intial commit

---
+++ src/EmptySlot.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+function EmptySlot() {
+    return (
+        <div>
+            <h2>EmptySlot</h2>
+        </div>
+    )
+}
+
+export default EmptySlot
---
+++ src/Mole.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+function Mole () {
+    return (
+        <div>
+            <h2>Mole</h2>
+        </div>
+    )
+}
+
+export default Mole;
